ffdd267a0e591e363aed7674d821e942e5258a6b
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2018-05-06 15:30:13 +0530

    adding boilerplate code. these may change in future.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { AuthenticationService } from './services/authentication.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
