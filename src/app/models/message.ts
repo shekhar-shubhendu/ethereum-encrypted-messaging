f9097c766a824d20da943ea0d9d7fd88c99ffad3
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2018-05-14 16:40:26 +0530

    added MessageType enum

--- src/app/models/message.ts
+++ src/app/models/message.ts
@@ -1,2 +1,9 @@
+import { MessageType } from '../enums/message-type.enum';
+
 export interface Message {
+  sender: string;
+  receiver: string;
+  encryptedMessage?: string;
+  actualMessage?: string;
+  messageType: MessageType;
 }
